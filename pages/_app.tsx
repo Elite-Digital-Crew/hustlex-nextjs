96b21c838d45b3e10a9ece8e0755f5e2f55b5410
DevSamuelV <realalloutmusic@gmail.com>
2021-12-24 13:15:01 -0500

    Added google analytics and added some padding to the SubHero text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,23 @@ import "../styles/DesktopStyling.css";
 import "../styles/phoneStyling.css";
 import "tailwindcss/tailwind.css";
 
+import ReactGa from "react-ga";
+import Head from "next/head";
+
 import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import Head from "next/head";
 
 export const client = new ApolloClient({
 	uri: "https://india.hustlex.club/",
 	cache: new InMemoryCache(),
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+ReactGa.initialize("G-HLK7XTH9J3");
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+	ReactGa.pageview(router.pathname);
+
 	return (
 		<ChakraProvider>
 			<Head>
--- src/components/subHero.tsx
+++ src/components/subHero.tsx
@@ -3,22 +3,23 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 
 interface Props {
-    title: string;
-    discription: string;
-    img: string;
+	title: string;
+	discription: string;
+	img: string;
 }
 
-const SubHero = ( props: Props ) => {
-    return (
-        <div className={styles.subhero} id="feature">
-        <div className={styles.subheroleft}>
-            <h1>{props.title}</h1>
-            <h2>{props.title}</h2>
-            <h3>{props.discription}</h3> 
-        </div>
-            <img src={props.img} alt="" className={styles.subheroimg}/>
-        </div>
-    )
+const SubHero = (props: Props) => {
+	return (
+		<div className={styles.subhero} id="feature">
+			<div className={styles.subheroleft}>
+				<h1>{props.title}</h1>
+				<h2>{props.title}</h2>
+
+				<h3 className="mt-3">{props.discription}</h3>
+			</div>
+			<img src={props.img} alt="" className={styles.subheroimg} />
+		</div>
+	);
 };
 
 export default SubHero;
