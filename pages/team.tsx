0b08e10d0b375e275e1303724ea30d4261d191d5
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2022-02-04 21:52:51 -0500

    Swaped the a attrbute with link

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import { Footer } from "../src/components/footer";
 import Hero from "../src/components/home";
 import NavBar from "../src/components/navBar";
@@ -14,27 +15,54 @@ const Team: NextPage = () => {
 			<div className={styles.hero}>
 				<div className={styles.content}>
 					<h1>people building HustleX</h1>
-					<p>Get to know more about us, what we do. Let us know if you would like to work with us.</p>
+					<p>
+						Get to know more about us, what we do. Let us know if you would like
+						to work with us.
+					</p>
 				</div>
 			</div>
 			<div className={styles.teamContainer}>
-				<Member name="Chinmay P Dev" role="Founder & CEO" img="./team-1.png" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-				<Member name="Ritesh Parmar" role="Product Designer" img="./team-2.png" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-				<Member name="Samuel Villegas" role="Tech Lead" img="./team-3.png" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-				<Member name="Priyanshu Dutta" role="Business" img="./team-4.png" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+				<Member
+					name="Chinmay P Dev"
+					role="Founder & CEO"
+					img="./team-1.png"
+					discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				/>
+				<Member
+					name="Ritesh Parmar"
+					role="Product Designer"
+					img="./team-2.png"
+					discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				/>
+				<Member
+					name="Samuel Villegas"
+					role="Tech Lead"
+					img="./team-3.png"
+					discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				/>
+				<Member
+					name="Priyanshu Dutta"
+					role="Business"
+					img="./team-4.png"
+					discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				/>
 			</div>
 			<div className={styles.whyContainer}>
 				<div className={styles.left}>
 					<h1>Why you should join us?</h1>
-					<p>we are gen-z minds working on a problem, encouraging team that craves to execute. working for a greater cause and real issue. we are looking for people who always want to hustle!!!</p>
+					<p>
+						we are gen-z minds working on a problem, encouraging team that
+						craves to execute. working for a greater cause and real issue. we
+						are looking for people who always want to hustle!!!
+					</p>
 				</div>
 				<div className={styles.right}>
-					<img src={'./team-join.png'} alt="" className={styles.whyimg}/>
+					<img src={"./team-join.png"} alt="" className={styles.whyimg} />
 				</div>
 			</div>
 			<div className={styles.jobcta}>
 				<h1>Checkout the open positions</h1>
-				<a href="/career">Take me there</a>
+				<Link href="/career">Take me there</Link>
 			</div>
 			<PreRegister />
 			<Footer />
