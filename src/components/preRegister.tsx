197c3307402aa1893ad06bf7690270d05fd7eedf
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2022-07-07 12:58:31 -0400

    added new preregister handling

--- src/components/preRegister.tsx
+++ src/components/preRegister.tsx
@@ -39,27 +39,39 @@ export const PreRegister = forwardRef<HTMLInputElement>((_, ref) => {
 		}
 
 		setLoading(true);
-		await client.query<PRE_REGISTER_QUERY_RESULT>({
+		const res = client.query<PRE_REGISTER_QUERY_RESULT>({
 			query: PRE_REGISTER_QUERY,
 			variables: {
 				email,
 			},
 		});
-		toast({
-			title: "Congratulations",
-			description: "You are now signed up!.",
-			status: "success",
-		});
-		setLoading(false);
+
+		await res
+			.then(() =>
+				toast({
+					title: "Congratulations",
+					description: "You are now signed up!",
+					status: "success",
+				})
+			)
+			.catch(({ message }) => {
+				toast({
+					title: "Error",
+					description: message,
+					status: "error",
+				});
+			})
+			.finally(() => setLoading(false));
 	};
 
 	return (
 		<div ref={ref} className={styles.container} id="register">
-			<div className={styles.left} >
+			<div className={styles.left}>
 				<div>
 					<h1>join club</h1>
 					<p className="mt-3">
-						join hustle club and be the first ones to acess the best fitness experience ever created. 
+						join hustle club and be the first ones to acess the best fitness
+						experience ever created.
 					</p>
 				</div>
 				<span className={styles.span}>
@@ -75,43 +87,43 @@ export const PreRegister = forwardRef<HTMLInputElement>((_, ref) => {
 						className={styles.button}
 						onClick={() => preRegister()}
 					>
-						{loading ? 
-						<svg
-							className="h-9"
-							version="1.1"
-							id="L9"
-							xmlns="http://www.w3.org/2000/svg"
-							xmlnsXlink="http://www.w3.org/1999/xlink"
-							x="0px"
-							y="0px"
-							viewBox="0 0 100 100"
-							enableBackground="new 0 0 0 0"
-							xmlSpace="preserve"
-						>
-							<path
-								fill="#000"
-								d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+						{loading ? (
+							<svg
+								className="h-9"
+								version="1.1"
+								id="L9"
+								xmlns="http://www.w3.org/2000/svg"
+								xmlnsXlink="http://www.w3.org/1999/xlink"
+								x="0px"
+								y="0px"
+								viewBox="0 0 100 100"
+								enableBackground="new 0 0 0 0"
+								xmlSpace="preserve"
 							>
-								<animateTransform
-									attributeName="transform"
-									attributeType="XML"
-									type="rotate"
-									dur="1s"
-									from="0 50 50"
-									to="360 50 50"
-									repeatCount="indefinite"
-								/>
-							</path>
-						</svg>
-							:
-						<img src="./Arrow.svg" alt="icon" />
-						}
+								<path
+									fill="#000"
+									d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+								>
+									<animateTransform
+										attributeName="transform"
+										attributeType="XML"
+										type="rotate"
+										dur="1s"
+										from="0 50 50"
+										to="360 50 50"
+										repeatCount="indefinite"
+									/>
+								</path>
+							</svg>
+						) : (
+							<img src="./Arrow.svg" alt="icon" />
+						)}
 					</button>
 				</span>
 			</div>
 			<div className={styles.mockup}>
 				<img src="/UI-MOCKUP-1.png" alt="mockup" />
-			</div>	
+			</div>
 		</div>
 	);
 });
