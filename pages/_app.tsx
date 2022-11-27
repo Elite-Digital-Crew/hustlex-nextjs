b66f53cc5ad9bbab8d64225753e9f926610bb98a
devsamuelv <realalloutmusic@gmail.com>
2022-11-27 15:02:40 -0500

    Changed the api region to asia

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,14 @@ import "tailwindcss/tailwind.css";
 
 import ReactGa from "react-ga";
 import Head from "next/head";
-import { usePostHog } from 'next-use-posthog'
+import { usePostHog } from "next-use-posthog";
 
 import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export const client = new ApolloClient({
-	uri: "https://api.hustlex.club",
+	uri: "https://asia.hustlex.club",
 	cache: new InMemoryCache(),
 });
 
@@ -18,8 +18,10 @@ ReactGa.initialize("G-HLK7XTH9J3");
 
 function MyApp({ Component, pageProps, router }: AppProps) {
 	ReactGa.pageview(router.pathname);
-	usePostHog('phc_DvbpQihwYVGPNC5SPhhOFuO0yJKJPEVt99G3DGhiM1x', { api_host: 'https://app.posthog.com' })
-	
+	usePostHog("phc_DvbpQihwYVGPNC5SPhhOFuO0yJKJPEVt99G3DGhiM1x", {
+		api_host: "https://app.posthog.com",
+	});
+
 	return (
 		<ChakraProvider>
 			<Head>
