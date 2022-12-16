0af2414011ed48c686281d28d5e9b0a2b1f699a7
ChinmayX <chinmaysjr26@gmail.com>
2022-12-16 12:45:09 +0530

    Revert "changed server url"
    This reverts commit f4a63afa5dff9b77c4275b490a07d35b62dcb067.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,26 +10,26 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export const client = new ApolloClient({
-  uri: "https://api.hustlex.club",
-  cache: new InMemoryCache(),
+	uri: "https://asia.hustlex.club",
+	cache: new InMemoryCache(),
 });
 
 ReactGa.initialize("G-HLK7XTH9J3");
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  ReactGa.pageview(router.pathname);
-  usePostHog("phc_DvbpQihwYVGPNC5SPhhOFuO0yJKJPEVt99G3DGhiM1x", {
-    api_host: "https://app.posthog.com",
-  });
+	ReactGa.pageview(router.pathname);
+	usePostHog("phc_DvbpQihwYVGPNC5SPhhOFuO0yJKJPEVt99G3DGhiM1x", {
+		api_host: "https://app.posthog.com",
+	});
 
-  return (
-    <ChakraProvider>
-      <Head>
-        <script async src="https://cdn.splitbee.io/sb.js"></script>
-      </Head>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider>
+			<Head>
+				<script async src="https://cdn.splitbee.io/sb.js"></script>
+			</Head>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
 export default MyApp;
