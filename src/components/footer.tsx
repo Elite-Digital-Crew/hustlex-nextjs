76d415c957b034d0901ec28ba31c3b43cd2cb40a
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2024-11-20 15:29:30 -0500

    cleared all the nextjs errors

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,68 +4,68 @@ import styles from "../../styles/Footer.module.css";
 // import { IconName } from "react-icons/io";
 
 interface LinkProps {
-  name: string;
-  href: string;
+	name: string;
+	href: string;
 }
 interface SocialLinkProps {
-  name: string;
-  href: string;
+	name: string;
+	href: string;
 }
 
 const Link = (props: LinkProps) => (
-  <a href={props.href} className={styles.link}>
-    <h1>{props.name}</h1>
-    <Image width={16} height={16} src="/icon.svg" alt="" />
-  </a>
+	<a href={props.href} className={styles.link}>
+		<h1>{props.name}</h1>
+		<Image width={16} height={16} src="/icon.svg" alt="" />
+	</a>
 );
 const SocialLink = (props: SocialLinkProps) => (
-  <a href={props.href} className={styles.socialLink}>
-    <Image width={54} height={54} src={props.name} alt={props.name} />
-  </a>
+	<a href={props.href} className={styles.socialLink}>
+		<Image width={54} height={54} src={props.name} alt={props.name} />
+	</a>
 );
 
 export const Footer = () => {
-  return (
-    <div className={styles.parent} id="footer">
-      <div className={styles.div1}>
-        <img src="/logo-sm.png" alt="" className={styles.logosm} />
-        <div>
-          <SocialLink
-            href="https://twitter.com/hustlexclub"
-            name="/twitter.png"
-          />
-          <SocialLink
-            href="https://www.instagram.com/hustlex.club/"
-            name="/instagram.png"
-          />
-          <SocialLink
-            href="mailto:chinmaypdev.offical@gmail.com"
-            name="/mail.png"
-          />
-          <SocialLink
-            href="https://www.linkedin.com/company/hustlexclub/"
-            name="/linkdinLogo.png"
-          />
-        </div>
-        <h1>© 2022 | HustleX Fitness.</h1>
-      </div>
-      <div className={styles.div2}>
-        <Link name="About" href="/about" />
-        <Link name="Team" href="/team" />
-        <Link name="Carrer" href="/career" />
-        {/* <Link name="Pitch Deck" href="/pitch-deck.pdf" /> */}
-      </div>
-      <div className={styles.div3}>
-        <Link name="Terms" href="/terms" />
-        <Link name="Privacy policy" href="/privacy" />
-        <Link
-          name="Get access"
-          href="https://play.google.com/store/apps/details?id=com.hustlex.mvp"
-        />
-      </div>
-      <div className={styles.div4}>
-        <p>#22/58, Rajajinagar, Bangalore - 560010</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.parent} id="footer">
+			<div className={styles.div1}>
+				<img src="/logo-sm.png" alt="" className={styles.logosm} />
+				<div>
+					<SocialLink
+						href="https://twitter.com/hustlexclub"
+						name="/twitter.png"
+					/>
+					<SocialLink
+						href="https://www.instagram.com/hustlex.club/"
+						name="/instagram.png"
+					/>
+					<SocialLink
+						href="mailto:chinmaypdev.offical@gmail.com"
+						name="/mail.png"
+					/>
+					<SocialLink
+						href="https://www.linkedin.com/company/hustlexclub/"
+						name="/linkdinLogo.png"
+					/>
+				</div>
+				<h1>© 2022 | HustleX Fitness.</h1>
+			</div>
+			<div className={styles.div2}>
+				<Link name="About" href="/about" />
+				<Link name="Team" href="/team" />
+				<Link name="Carrer" href="/career" />
+				{/* <Link name="Pitch Deck" href="/pitch-deck.pdf" /> */}
+			</div>
+			<div className={styles.div3}>
+				<Link name="Terms" href="/terms" />
+				<Link name="Privacy policy" href="/privacy" />
+				<Link
+					name="Get access"
+					href="https://play.google.com/store/apps/details?id=com.hustlex.mvp"
+				/>
+			</div>
+			<div className={styles.div4}>
+				<p>#22/58, Rajajinagar, Bangalore - 560010</p>
+			</div>
+		</div>
+	);
 };
